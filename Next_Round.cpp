c0957a2d7a46a06e8f93a9325ce804756ab5fed0
Navin-Nawar <navinvau17@gmail.com>
2026-06-25 12:36:20 +0600

    solved new problem

---
+++ Next_Round.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n,k,i,count=0,j;
+    cin>>n>>k;
+    int a[n];
+    for(i=0;i<n;i++)
+    {
+        cin>>a[i];
+    }
+    for(i=0;i<n;i++)
+    {
+        if(a[i]>0)
+        {
+        if(a[i]>=a[k-1])
+        count++;
+        }
+    }
+   
+    cout<<count<<endl;
+    return 0;
+}
