ff3c90ecf49a676cb3460fcfd76aff3ffc715a54
Navin-Nawar <navinvau17@gmail.com>
2026-06-28 09:14:30 +0600

    solved new problem

---
+++ Watermelon.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i;
+    cin>>i;
+    
+        if(i>=4 && i%2==0)
+        cout<<"YES";
+        else 
+        cout<<"NO";
+    
+    return 0;
+}
