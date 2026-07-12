6024a21e2d117981c13a61720601c5e8b7180093
Navin-Nawar <navinvau17@gmail.com>
2026-07-12 18:43:59 +0600

    solved new problem

---
+++ A_Calculating_Function.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fastio ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define ll long long
+#define pb push_back
+#define print(x) cout << x << '\n'
+#define prints(x) cout << x << ' '
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+#define f(i,a,b) for(int i = a; i < b; i++)
+#define fr(i,a,b) for(int i = a; i >= b; i--)
+
+void solve() {
+    ll n;cin>>n;
+   if(n%2==0){
+    print(n/2);
+   }
+   else print(-((n+1)/2));
+}
+
+int main() {
+    fastio;
+    solve();
+    return 0;
+}
