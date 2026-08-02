4662955ff3e990fb666545b8656768e8d79dc381
Navin-Nawar <navinvau17@gmail.com>
2026-08-02 10:46:50 +0600

    solved new problem

---
+++ A_Buying_Torches.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fastio ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define ll long long
+#define pb push_back
+#define print(x) cout << x << '\n'
+#define prints(x) cout << x << ' '
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+#define f(i,a,b) for(int i = a; i < b; i++)
+#define fr(i,a,b) for(int i = a; i >= b; i--)
+
+void solve() {
+    ll x,y,k;
+    cin>>x>>y>>k;
+   ll need = k + y * k - 1;
+   ll ans = (need + (x - 2)) / (x - 1) + k;
+    cout<<ans<<endl;
+}
+
+int main() {
+    fastio;
+    int t;
+    cin >> t;
+    while(t--) solve();
+    return 0;
+}
