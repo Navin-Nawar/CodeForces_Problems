318e7844299fad9d28fa00b226023402559e1f46
Navin-Nawar <navinvau17@gmail.com>
2026-08-08 00:08:37 +0600

    solved new problem

---
+++ A_The_Best_Card.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fastio ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define ll long long
+#define pb push_back
+#define print(x) cout << x << '\n'
+#define prints(x) cout << x << ' '
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+#define f(i,a,b) for(int i = a; i < b; i++)
+#define fr(i,a,b) for(int i = a; i >= b; i--)
+
+void solve() {
+    int n;cin>>n;
+    if(n%2!=0)no;
+    else{
+        for(int i=3;i<=n/2;i+=2){
+            if((n+1)%i==0){
+                no;return;
+            }  
+        }
+
+        yes;
+    }
+}
+
+int main() {
+    fastio;
+    int t;
+    cin >> t;
+    while(t--) solve();
+    return 0;
+}
