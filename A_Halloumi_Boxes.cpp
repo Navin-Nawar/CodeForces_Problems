7c5f80017b77574ce4d98d40c3402e2744a1626a
Navin-Nawar <navinvau17@gmail.com>
2026-08-07 16:49:15 +0600

    solved new problem

---
+++ A_Halloumi_Boxes.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fastio ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define ll long long
+#define pb push_back
+#define print(x) cout << x << '\n'
+#define prints(x) cout << x << ' '
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+#define f(i,a,b) for(int i = a; i < b; i++)
+#define fr(i,a,b) for(int i = a; i >= b; i--)
+
+void solve() {
+    int n,k;
+    cin>>n>>k;
+    vector<int>v(n),vprev(n);
+    f(i,0,n)cin>>v[i];
+    vprev=v;
+
+    sort(all(v));
+
+    if(v==vprev){
+        yes;
+        
+    }
+
+    else if(k>1)yes;
+    else no;
+}
+
+int main() {
+    fastio;
+    int t;
+    cin >> t;
+    while(t--) solve();
+    return 0;
+}
